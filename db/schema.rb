53230a3a5d4a65ea8b83a003e97167437d7101af
Paul Louis Fouesnant <fouesnant_paul@hotmail.com>
2020-02-01 16:56:21 +0100

    modele User_consultation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_144930) do
+ActiveRecord::Schema.define(version: 2020_02_01_154305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2020_02_01_144930) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_consultations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "consultation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["consultation_id"], name: "index_user_consultations_on_consultation_id"
+    t.index ["user_id"], name: "index_user_consultations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,4 +76,6 @@ ActiveRecord::Schema.define(version: 2020_02_01_144930) do
 
   add_foreign_key "consultations", "patients"
   add_foreign_key "glucose_levels", "patients"
+  add_foreign_key "user_consultations", "consultations"
+  add_foreign_key "user_consultations", "users"
 end
---
+++ test/models/user_consultation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserConsultationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
