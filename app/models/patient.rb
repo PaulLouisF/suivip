a43329cba35f42d37565a71a379f359dc0e056cc
Paul Louis Fouesnant <fouesnant_paul@hotmail.com>
2020-02-02 23:16:40 +0100

    validation des modeles

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,8 @@
 class Patient < ApplicationRecord
+  has_many :consultations
+  has_many :glucose_levels
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birthdate, presence: true
+  validates :gender, presence: true
 end
