84f456a8e4f38f88a3d0cd12f9ab54ddb2801c17
Saïf <saifbacari@gmail.com>
2020-02-08 15:01:52 +0100

    form patient ok + css form crée

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -40,6 +40,6 @@ class PatientsController < ApplicationController
   end
 
   def patient_params
-    params.require(:patient).permit(:first_name, :last_name, :birthdate, :gender)
+    params.require(:patient).permit(:first_name, :last_name, :birthdate, :gender, :address, :phone_number, :city)
   end
 end
