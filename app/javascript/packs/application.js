7fd9866dec6c11c50b8e45a6cfeb3f26b4a85912
juliends <julien@lewagon.org>
2020-02-08 09:40:57 +0100

    charkick demo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+import Chartkick from "chartkick"
+import Chart from "chart.js"
+
+const glucoseChart = document.getElementById('glucose-chart');
+// Ne lancer le graph que si la div glucoseChart existe sur la page
+if (glucoseChart) {
+  Chartkick.use(Chart)
+  // Data contient les coordonés de chaque point.
+  // Il faudra les récupérer du controlleur puis les cacher dans le HTML comme des coordonnées de map
+  const data = [ [0, 300], [10, 100], [40, 200], [100, 600] ];
+  new Chartkick.ScatterChart("glucose-chart", data);
+}
