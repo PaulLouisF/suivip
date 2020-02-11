a787751464943332ac1f11f08ba0bd899e955628
Ziad Ghalleb <ziad.ghalleb@protonmail.com>
2020-02-11 20:41:28 +0100

    added patients layout - controllers need more work to integrate alyout

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,6 +1,6 @@
 class ConsultationsController < ApplicationController
-    before_action :authenticate_user!
-    skip_after_action :verify_policy_scoped, only: :index
+  before_action :authenticate_user!
+  skip_after_action :verify_policy_scoped, only: :index
 
   def index
   end
