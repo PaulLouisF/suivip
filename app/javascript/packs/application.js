8f0168c171dbffa087700598a5122080a8032c41
Paul Louis Fouesnant <fouesnant_paul@hotmail.com>
2020-02-25 21:48:01 +0100

    chart doughnut

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import Chart from "chart.js"
 
 var ctx = document.getElementById('glucoseChart');
 
-const cleanSet  = () => {
+const cleanSet  = (newPoint) => {
   var values = JSON.parse(document.getElementById('essaiT').dataset.dates_enable);
   console.log(values);
 
@@ -36,21 +36,28 @@ const cleanSet  = () => {
 
 
 if (ctx) {
-  const reloadGraph = () => {
+  const reloadGraph = (newPoint) => {
     var glucoseChart = new Chart(ctx, {
         type: 'scatter',
         data: {
             datasets: [{
-                label: 'Glycémies',
-                data: cleanSet(),
+                // label: 'Glycémies',
+                data: cleanSet(3),
                 pointRadius: 4,
-                pointBackgroundColor: 'red',
+                pointBackgroundColor: 'blue',
             }]
         },
         options: {
+          legend: {position: 'bottom', display: false},
             scales: {
                 xAxes: [{
                     type: 'time',
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Heures',
+                      fontSize: 16
+                    },
+
                     time: {
                        unit: 'hour',
                        displayFormats: {
@@ -58,19 +65,33 @@ if (ctx) {
                         }
                     },
                     position: 'bottom'
+                }],
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  },
+                  scaleLabel: {
+                      display: true,
+                      labelString: 'Glycémies (mg/dL)',
+                      fontSize: 16
+                    },
                 }]
             }
         }
     });
+  }
+
+    // reloadGraph();
 
     // console.log(glucoseChart.data.datasets);
 
-    // var datat = {x: new Date ('2020-2-29 8:00'), y: 700};
+    // var datat = {x: new Date ('2020-2-29 23:58'), y: 700};
     // console.log(datat);
 
-    // const updateGraph = () => {
+    // // const updateGraph = () => {
     //   glucoseChart.data.labels.push('hello');
     //   console.log("toto1");
+
     //   glucoseChart.data.datasets.forEach((dataset) => {
     //       dataset.data.push(datat);
     //   });
@@ -82,16 +103,59 @@ if (ctx) {
 
     // updateGraph();
 
-  }
+
   reloadGraph();
 
 
+  window.MyLib = {};
+  MyLib.reloadGraph = reloadGraph();
+}
 
 
 
 
 
+var cty = document.getElementById('glucosePieChart');
+
+
+const cleanPieSet  = (newPoint) => {
+  var values = JSON.parse(document.getElementById('essaiT').dataset.dates_enable);
+
+  const result1 = values.filter(element => element.y < 70).length;
+  const result2 = values.filter(element => element.y > 70 && element.y < 180).length;
+  const result3 = values.filter(element => element.y > 180).length;
+
+  var glucoseBreakDown = [result1, result2, result3];
+
+
+
+
+  return glucoseBreakDown
+};
+
+
+if (cty) {
+  const reloadPieGraph = (newPoint) => {
+
+
+    var myDoughnutChart = new Chart(cty, {
+        type: 'doughnut',
+        data: {
+          datasets: [{
+            data: cleanPieSet(),
+            backgroundColor: ["#0074D9", "#FF4136", "#2ECC40"]
+          }],
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+          position: 'bottom',
+          labels: ['<70mg/dL', '70-180 mg/dL', '>180 mg/dL'],
+        },
+        options: {
+          legend: {position: 'bottom'}
+        }
+      });
+    }
+  reloadPieGraph();
   // window.MyLib = {};
-  // MyLib.reloadGraph = reloadGraph;
+  // MyLib.reloadPieGraph = reloadPieGraph();
 }
 
