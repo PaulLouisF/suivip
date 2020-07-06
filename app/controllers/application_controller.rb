3de5146d23093764f7f44274a9e6565eef675623
Paul Louis Fouesnant <fouesnant_paul@hotmail.com>
2020-07-06 17:31:50 +0200

    debbug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :specialist])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :specialist])
+  # end
 
 
   before_action :authenticate_user!
